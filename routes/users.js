d63aa6547f28ff9c2b683fb801c41a3cd151a25f
sporodreki <mohamed.mardhi@gmail.com>
2021-04-11 22:57:57 +0200

    adding jwt - user registration completed

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt    = require('jsonwebtoken')
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
+const config = require('config');
 
 // @route   POST api/users
 // @desc    Register a user
@@ -33,7 +35,18 @@ router.post('/',[
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
             await user.save();
-            res.send('User saved')
+            // jwt config
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            jwt.sign(payload, config.get('jwtSecret'), {
+                expiresIn: 36000,
+            }, (err, token)=> {
+                if(err) throw err
+                res.json({token})
+            })
 
             
         } catch (err) {
