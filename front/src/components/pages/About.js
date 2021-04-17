5b4ca27cf98ef6a55c00d33f28b42cf568b42d2d
sporodreki <mohamed.mardhi@gmail.com>
2021-04-18 01:32:41 +0200

    adding proxy to deal with backend and finish first comp Navbar

---
+++ front/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h3 className="my-1">Hello world ! it's my first MERN App ! cheers</h3>
+        </div>
+    )
+}
+
+export default About;
