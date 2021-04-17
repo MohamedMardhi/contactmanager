b6e0c342f08b36d2be8459132efc3129dc6eba2c
sporodreki <mohamed.mardhi@gmail.com>
2021-04-17 18:55:47 +0200

    adding Contact model and finish add new contact by user

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Contact manager
+
+## Settings
+if you want to run this project you need to create a cluster on mongodb, then paste it, into default.json
+
+`
+{
+    "mongoURI" : "mongodb+srv://login:password@cluster89.tuus9.mongodb.net/DATABASE?retryWrites=true&w=majority"
+}
+`
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+const auth = require('../middleware/auth');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // access   PRIVATE
-router.get('/', (req, res) => {
-    res.send('get all contacts');
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
+        res.json(contacts);
+    } catch (err) {
+        res.status(500).send('Something goes wrong with our server');
+    }
 });
 
 // @route   POST api/contacts
 // @desc    Add new contacts
 // access   PRIVATE
-router.post('/', (req, res) => {
-    res.send('add contact');
+router.post('/', [auth,[
+    check('name', 'name is required')
+    .not()
+    .isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    const {name, email, phone, type} = req.body;
+    try {
+        const newContact = new Contact ({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        })
+        const contact = await newContact.save();
+
+        res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Something goes wrong with our server');
+    }
 });
 
 // @route   PUT api/contacts/:id
